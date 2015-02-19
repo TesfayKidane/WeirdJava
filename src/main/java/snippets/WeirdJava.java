20e8625cb8b4d52bf4fa9674974bd507b542acc7
Tesfay <t.aregay@innovadisgroep.com>
2015-02-19 11:04:58 +0100

    second commit

--- src/main/java/snippets/WeirdJava.java
+++ src/main/java/snippets/WeirdJava.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package nl.indenty.snippets;
+package snippets;
 
+import nl.indenty.snippets.*;
 import java.util.Random;
 
 /**
